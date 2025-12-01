6e9fa71049849f2dc000cb59ed21e289fb2ea4aa
JuhoKarppanen <bhq663@myy.haaga-helia.fi>
2025-12-02 01:46:03 +0200

    Tehty kaksi sivua ja reititys niiden välille

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Link, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import './index.css'
--- src/pages/TrainingList.tsx
+++ src/pages/TrainingList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import{ useEffect, useMemo, useState } from 'react'
 import { fetchTrainingsWithCustomer } from '../api'
 import type { TrainingWithCustomer } from '../types'
 import dayjs from 'dayjs'
