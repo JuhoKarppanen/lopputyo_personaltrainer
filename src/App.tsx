e30ac9180f081ea4b31028a7a74e7f0aea5f72e8
JuhoKarppanen <bhq663@myy.haaga-helia.fi>
2025-12-04 21:58:43 +0200

    Päivitetty navigaatio

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import './index.css'
 import CustomerList from './pages/CustomerList'
 import TrainingList from './pages/TrainingList'
+import CalendarPage from './pages/CalendarPage'
 import ErrorBoundary from './components/ErrorBoundary'
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
         <h1>Personal Trainer - Admin</h1>
         <nav style={{ marginBottom: 16 }}>
           <Link to="/customers" style={{ marginRight: 12 }}>Customers</Link>
-          <Link to="/trainings">Trainings</Link>
+          <Link to="/trainings" style={{ marginRight: 12 }}>Trainings</Link>
+          <Link to="/calendar">Calendar</Link>
         </nav>
       </header>
 
@@ -23,6 +25,7 @@ function App() {
             <Route path="/" element={<Navigate to="/customers" replace />} />
             <Route path="/customers" element={<CustomerList />} />
             <Route path="/trainings" element={<TrainingList />} />
+            <Route path="/calendar" element={<CalendarPage />} />
           </Routes>
         </ErrorBoundary>
       </main>
